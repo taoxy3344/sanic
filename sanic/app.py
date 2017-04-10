241b68c085fdb9189715ae1cf2d9676675d67662
taoxy3344 <taoxy3344@users.noreply.github.com>
2017-04-10 19:03:05 +0800

    Update app.py

--- sanic/app.py
+++ sanic/app.py
@@ -283,6 +283,7 @@ class Sanic:
             return register_middleware(middleware_or_request)
 
         else:
+            #关于偏函数partial的教程：http://www.liaoxuefeng.com/wiki/001374738125095c955c1e6d8bb493182103fac9270762a000/001386819893624a7edc0e3e3df4d5d852a352b037c93ec000
             return partial(register_middleware,
                            attach_to=middleware_or_request)
 
